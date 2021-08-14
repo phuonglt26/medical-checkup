f20e4fab92f825b928c6501032f732c0ea8bf44d
Phuong <anhphuong372000@gmail.com>
2021-08-14 23:55:33 +0700

    update

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./style.scss";
-import logo from "./logo.svg";
-import background from "./background.svg";
 const Home: React.FC = () => {
   return (
     <div className="medical-checkup">
@@ -30,9 +28,28 @@ const Home: React.FC = () => {
       <div className="license" id="step2">
         <div className="body-step2">
           <div className="title-step2">Thông tin cá nhân</div>
-          <div className="name-input">
+          <div className="name-input column">
             <div className="name">Họ và Tên: </div>
-            <input className="name" type="text" />
+            <input className="name input" type="text" />
+          </div>
+          <div className="age-input column">
+            <div className="age">Tuổi: </div>
+            <input className="age input" type="text" />
+          </div>
+          <div className="phone-input column">
+            <div className="phone">Phone: </div>
+            <input className="phone input" type="text" />
+          </div>
+          <div className="sex-input column">
+            <div className="sex">Giới tính: </div>
+            <div className="sex button-sex">
+              <div className="button"></div>
+              <div className="button end"></div>
+            </div>
+            <div className="img-sex">
+              <div className="girl"></div>
+              <div className="man"></div>
+            </div>
           </div>
         </div>
       </div>
