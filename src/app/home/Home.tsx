cf608c8273de2b8a5fc4ccd83b4321b49e9d86bd
Phuong <anhphuong372000@gmail.com>
2021-08-15 01:36:37 +0700

    update 2

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -25,7 +25,7 @@ const Home: React.FC = () => {
           <button className="start-button">Get started</button>
         </a>
       </div>
-      <div className="license" id="step2">
+      <div className="information" id="step2">
         <div className="body-step2">
           <div className="title-step2">Thông tin cá nhân</div>
           <div className="name-input column">
@@ -50,6 +50,68 @@ const Home: React.FC = () => {
               <div className="girl"></div>
               <div className="man"></div>
             </div>
+            <div className="text-sex">
+              <div className="text">Nữ</div>
+              <div className="text">Nam</div>
+            </div>
+          </div>
+          <div className="switch-button">
+            <a href="#step1">
+              <button className="button">Trang trước</button>
+            </a>
+            <a href="#step3">
+              <button className="button end">Tiếp tục</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="choose-level" id="step3">
+        <div className="body-step3">
+          <div className="title-step3">Chọn nhóm bệnh liên quan</div>
+          <div className="choice-step">
+            <div className="choice">Covid-19</div>
+            <div className="choice">Bệnh thông thường</div>
+            <div className="choice">Bệnh nghiêm trọng</div>
+          </div>
+          <div className="switch-button">
+            <a href="#step2">
+              <button className="button">Trang trước</button>
+            </a>
+            <a href="#step4">
+              <button className="button end">Tiếp tục</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="choose-level" id="step4">
+        <div className="body-step4">
+          <div className="title-step4">Chọn vùng bị đau trên cơ thể</div>
+          <div className="choice-step">
+            <img src="./body.jpg" alt="" />
+          </div>
+          <div className="switch-button">
+            <a href="#step3">
+              <button className="button">Trang trước</button>
+            </a>
+            <a href="#step5">
+              <button className="button end">Tiếp tục</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="choose-level" id="step5">
+        <div className="body-step4">
+          <div className="title-step4">Kiểm tra bệnh lý nền</div>
+          <div className="choice-step">
+            <img src="./body.jpg" alt="" />
+          </div>
+          <div className="switch-button">
+            <a href="#step3">
+              <button className="button">Trang trước</button>
+            </a>
+            <a href="#step5">
+              <button className="button end">Tiếp tục</button>
+            </a>
           </div>
         </div>
       </div>
